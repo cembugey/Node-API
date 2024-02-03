d4ea56438c698978d62b0a3b87bf9636208e8e2a
Cem <cem.bugey@insharo.consulting>
2024-02-04 01:03:32 +0300

    .

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
@@ -10,7 +11,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/json', (req, res) => {
-    res.json({"message": "Hello json"});
+    res.json({"message": process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json"});
 })
 
 
